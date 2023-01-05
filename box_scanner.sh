ecde2f6357ac147152413e32eef3eee7bbeadfed
chan <1050181200@qq.com>
2023-01-05 09:58:12 +0800

    write function

--- box_scanner.sh
+++ box_scanner.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 # htb 基本信息收集
+# 1.
 # 输入一个ip 地址
 # 检查输入到是不是ip地址,如果不是就退出
 
+# 2.
 # curl --head | grep location
 # 如果没有域名 把域名存到 /etc/hosts
 # 如果有域名 ...
 
-# 进行 全端口扫描
+# 3.
+# 进行 全端口扫描 /ncat ? /nmap ? /rustscan ?
 # 枚举每个端口的banner
 
- 
+# 4.
+# 爬虫 抓取基本网页 连接 看看有不有可用的
+ADDRESS=''
+FULLSCAN=''
+
 function check_ip(){
         IP=$1
         VALID_CHECK=$(echo $IP|awk -F. '$1<=255 && $2<=255 && $3<=255 && $4<=255 {print "yes"}')
@@ -27,25 +34,39 @@ function check_ip(){
         fi
 }
 
+function check_input(){
 
 if [[ $# -lt 1 ]]
 then
-    echo '[-] please input an address'
-    echo '[-] be like bash box_scanner.sh 10.10.10.xxx'
-    exit
+        echo '[-] please input an address'
+        echo '[-] be like bash box_scanner.sh 10.10.10.xxx'
+        exit
 fi
 
 ADDRESS=$1
 FULLSCAN=$2
 check_ip $ADDRESS
 
+}
+
+
+function get_domain(){
+
 echo "[+]scaning for $ADDRESS"
 echo '[+] staring curl...'
-
 DOMAIN=$(curl $ADDRESS --head | grep 'Location' | cut -d " " -f 2) 
-
 echo "[+] get domain: $DOMAIN"
 # do you want export domain to /etc/host?
+}
+
+
+function get_url(){
+# get url from web page
+curl --url "$DOMAIN" | grep -E '(href|src)'
+}
+
+
+function port_scan(){
 
 if [[ $FULLSCAN == 'f' ]]
 then
@@ -60,3 +81,14 @@ for ((i=1 ; i <= $PORT; i++))
 do
     nc -nvv -w 1 -z $ADDRESS $i 2>&1 | grep 'succeeded'
 done
+}
+
+
+function main(){
+        check_input $1 $2
+        get_domain
+        get_url
+        port_scan
+}
+
+main $1 $2
